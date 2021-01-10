4683e8e610e818488cfb997a47c199f7961fb1f6
Sangsu Song <dvlprsong@gmail.com>
2021-01-10 23:09:54 +0900

    Add styled-reset to initialize all styles and create global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Router from './Router';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <div className="App">
       <Router />
+      <GlobalStyles />
     </div>
   );
 }
