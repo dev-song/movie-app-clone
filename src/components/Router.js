447ff4061c8088df28051c6ac0a5949a8bedb910
sssong <sangsu.song@aiceltech.com>
2021-02-07 02:13:46 +0900

    Implement DetailContainer, which shows details about movies and tv shows

--- src/components/Router.js
+++ src/components/Router.js
@@ -15,7 +15,7 @@ const Router = () => (
       <Route path='/tvshows' component={TVShows} />
       <Route path='/search' component={Search} />
       <Route path='/movie/:id' component={Detail} />
-      <Route path='/show/:id' component={Detail} />
+      <Route path='/tvshow/:id' component={Detail} />
       <Redirect from='*' to='/' />
     </Switch>
   </BrowserRouter>
--- src/routes/Detail/DetailContainer.js
+++ src/routes/Detail/DetailContainer.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import DetailPresenter from './DetailPresenter';
 
+import { MOVIE_API, TV_API } from '../../api';
+
 class DetailContainer extends React.Component {
-  state = {
-    result: null,
-    error: false,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
+    this.state = {
+      result: null,
+      error: false,
+      loading: true,
+      isMovie: pathname.includes('/movie/'),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push },
+    } = this.props;
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push('/');
+    }
+
+    let result = null;
+    const { isMovie } = this.state;
+    try {
+      ({ data: result } = isMovie
+        ? await MOVIE_API.movieDetail(id)
+        : await TV_API.TVDetail(id))
+    } catch {
+      this.setState({ error: `Can't find anything :(` })
+    } finally {
+      this.setState({ loading: false, result })
+    }
+  }
 
   render() {
     const { result, error, loading } = this.state;
