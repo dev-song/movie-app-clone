0d33efb40a5d6556650061cdb91b89344663c23c
dev-song <sangsu.song@aiceltech.com>
2021-02-11 23:43:58 +0900

    Create Poster component

--- src/routes/Home/HomePresenter.js
+++ src/routes/Home/HomePresenter.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Section from '../../components/Section';
 import Loader from '../../components/Loader';
 import Message from '../../components/Message';
+import Poster from '../../components/Poster';
 
 const Container = styled.div`
   padding: 0px 10px;
@@ -16,17 +17,17 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) => loa
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title='Now Playing'>
-          {nowPlaying.map(({ title, id }) => <span key={id}>{title}</span>)}
+          {nowPlaying.map(({ title, id }) => <Poster />)}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title='Upcoming Movies'>
-          {upcoming.map(({ title, id }) => <span key={id}>{title}</span>)}
+          {upcoming.map(({ title, id }) => <Poster />)}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title='Popular Movies'>
-          {popular.map(({ title, id }) => <span key={id}>{title}</span>)}
+          {popular.map(({ title, id }) => <Poster />)}
         </Section>
       )}
       {error && <Message text={error} color='#e74c3c' />}
